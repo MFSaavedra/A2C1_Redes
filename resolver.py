0c411aefab9efa2c43b08f4850b3c8bb1b04aa9e
mariaemg <maria.moya.g@ug.uchile.cl>
2026-08-29 15:32:18 -0400

    fix: minor errors in resolver.py

--- resolver.py
+++ resolver.py
@@ -4,16 +4,16 @@ import dnslib
 listen_address = ("0.0.0.0", 8000)
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-listen_address.bind(listen_address)
+listen_socket.bind(listen_address)
 
 try:
     while True:
         data, addr = listen_socket.recvfrom(4096)
-        print(f"Mensaje....blabla:")
+        print(f"Mensaje recibido desde {addr}:")
         print(data)
-except..:
+except KeyboardInterrupt:
     print("\nServidor detenido.")
 finally:
-    sock.close() 
+    listen_socket.close() 
 
 
